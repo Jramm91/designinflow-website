e4e18093ed39a9acd0ba43960dc45e652aade197
Jordan Rammelsberg <jordan.rammelsberg@gmail.com>
2025-05-12 21:23:00 -0500

    set up tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-import './App.css';
-
 function App() {
   return (
     <>
-      <p>Hello World</p>
-      <p>Hello World</p>
+      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
     </>
   );
 }
